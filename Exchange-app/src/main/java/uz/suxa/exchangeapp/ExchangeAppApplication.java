679c499d967de344f087c3ebf7e33b538c416298
suxa <suxa5637@mail.ru>
2022-10-12 22:43:54 +0500

    union projects

---
+++ Exchange-app/src/main/java/uz/suxa/exchangeapp/ExchangeAppApplication.java
@@ -0,0 +1,13 @@
+package uz.suxa.exchangeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeAppApplication.class, args);
+    }
+
+}
---
+++ Exchange-app/src/test/java/uz/suxa/exchangeapp/ExchangeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.suxa.exchangeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
